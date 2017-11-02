d00b09fceb4ca785195219bfc60eb6e3fb7e7326
WonWooS <songw@unbc.ca>
2017-11-02 14:41:02 -0700

    now, it will just ask name and auto load json files

--- GUI/gui.py
+++ GUI/gui.py
@@ -1,7 +1,7 @@
 from tkinter import *
 import tkinter as tk
-
-import FileIO.FileIO as fileIO
+from tkinter import messagebox
+from FileIO import FileIO
 from Components.User import User
 from Components.Answer import Answer
 from Components.WorkingMemory import WorkingMemory
@@ -11,110 +11,33 @@ from Components.KnowledgeBase import KnowledgeBase
 
 class Window:
     def __init__(self, master):
-        # create blank user, working memory, and knowledge base
         self.user = User()
         self.wm = WorkingMemory()
-        self.kb = KnowledgeBase()
+        self.rules = FileIO.load_rules('JSON/test_rules.json')
+        self.phones = FileIO.load_phones('JSON/phones.json')
+        self.questions = FileIO.load_questions('JSON/test_questions.json')
 
-        self.rules = []
-        self.rules_file = " "
-        rules_json = Label(root, text="Rules JSON").grid(row=1, column=0)
-        bar_placeholder1 = Entry(master, textvariable=self.rules_file).grid(row=1, column=2)
+        self.wm.set_user(self.user)
+        self.wm.set_rules(self.rules)
+        self.wm.set_phones(self.phones)
+        self.wm.set_questions(self.questions)
 
-        self.questions = []
-        self.questions_file = " "
-        questions_json = Label(root, text="Questions JSON").grid(row=2, column=0)
-        bar_placeholder2 = Entry(master, textvariable=self.questions_file).grid(row=2, column=2)
-
-        self.phones = []
-        self.phones_file = " "
-        phones_json = Label(root, text="Phones JSON").grid(row=3, column=0)
-        bar_placeholder2 = Entry(master, textvariable=self.phones_file).grid(row=3, column=2)
-
-        # First file buttons
-        self.cbutton = Button(root, text="Load Rules", command=self.load_rules)
-        self.cbutton.grid(row=1, column=4, sticky=W + E)
-        self.bbutton = Button(root, text="Browse", command=self.browse_rules_file)
-        self.bbutton.grid(row=1, column=1)
-
-        self.cbutton = Button(root, text="Load Questions", command=self.load_questions)
-        self.cbutton.grid(row=2, column=4, sticky=W + E)
-        self.bbutton = Button(root, text="Browse", command=self.browse_questions_file)
-        self.bbutton.grid(row=2, column=1)
-
-        self.cbutton = Button(root, text="Load Phones", command=self.load_phones)
-        self.cbutton.grid(row=3, column=4, sticky=W + E)
-        self.bbutton = Button(root, text="Browse", command=self.browse_phones_file)
-        self.bbutton.grid(row=3, column=1)
-
-        Label(master, text="Type your First Name").grid(row=4,column=0)
-        Label(master, text="Type your Last Name").grid(row=5,column=0)
+        Label(master, text="Type your First Name").grid(row=1,column=0)
+        Label(master, text="Type your Last Name").grid(row=2,column=0)
 
         self.e1 = Entry(master)
         self.e2 = Entry(master)
 
-        self.e1.grid(row=4, column=2)
-        self.e2.grid(row=5, column=2)
+        self.e1.grid(row=1, column=2)
+        self.e2.grid(row=2, column=2)
 
         self.bbutton = Button(root, text="Display Questions", command=self.display_questions)
-        self.bbutton.grid(row=2, column=5)
-
-    def browse_rules_file(self):
-        from tkinter.filedialog import askopenfilename
-        first_file_path = StringVar()
-        first_bar = Entry(root, textvariable=first_file_path)
-        first_bar.grid(row=1, column=2)
-
-        Tk().withdraw()
-        self.rules_file = askopenfilename()
-        first_file_path.set(self.rules_file)
-
-    def browse_questions_file(self):
-        from tkinter.filedialog import askopenfilename
-        second_file_path = StringVar()
-        second_bar = Entry(root, textvariable=second_file_path)
-        second_bar.grid(row=2, column=2)
-
-        Tk().withdraw()
-        self.questions_file = askopenfilename()
-        second_file_path.set(self.questions_file)
-
-    def browse_phones_file(self):
-        from tkinter.filedialog import askopenfilename
-        third_file_path = StringVar()
-        third_bar = Entry(root, textvariable=third_file_path)
-        third_bar.grid(row=3, column=2)
-
-        Tk().withdraw()
-        self.phones_file = askopenfilename()
-        third_file_path.set(self.phones_file)
-
-    def load_rules(self):
-        if self.rules_file:
-            self.rules = fileIO.load_rules(self.rules_file)
-            # print(rules)
-
-            # Store rules in Knowledge Base
-            self.kb.set_rules(self.rules)
-            self.wm.set_rules(self.kb.get_rules())
-
-    def load_questions(self):
-        if self.questions_file:
-            self.questions = fileIO.load_questions(self.questions_file)
-            # print(questions)
-
-            self.wm.set_questions(self.questions)
-
-    def load_phones(self):
-        if self.phones_file:
-            self.phones = fileIO.load_questions(self.phones_file)
-            # print(self.phones)
-
-            self.wm.set_phones(self.phones)
+        self.bbutton.grid(row=3, column=1)
 
     def display_questions(self):
         self.user.set_user_first_name(self.e1.get())
         self.user.set_user_last_name(self.e2.get())
+        print(self.user)
         window = tk.Toplevel(root)
         window.minsize(width=666, height=666)
         window.maxsize(width=666, height=666)
@@ -148,8 +71,8 @@ class Window:
                 ans = Answer(user_topic, user_answer)
                 user.set_answer(ans)
                 print(user_topic, user_answer)
-
-        print(user.get_answers())
+        messagebox.showinfo('Result', user.get_answers())
+        #print(user.get_answers())
 
 root = Tk()
 root.title("Cellphone selector")
--- testing.py
+++ testing.py
@@ -4,7 +4,9 @@ from Components.User import User
 from Components.Answer import Answer
 from Components.WorkingMemory import WorkingMemory
 from Components.InferenceEngine import InferenceEngine
+from Components.KnowledgeBase import KnowledgeBase
 from GUI import gui
+
 '''
 def test_file_io():
     FileIO.load_rules('JSON/test_rules.json')
@@ -15,20 +17,30 @@ def test_file_io():
 
 
 def test_process():
+    # create blank user, working memory, and knowledge base
     user = User()
     wm = WorkingMemory()
+    kb = KnowledgeBase()
+
+    # Import rules, phones, and questions
     rules = FileIO.load_rules('JSON/test_rules.json')
     phones = FileIO.load_phones('JSON/phones.json')
     questions = FileIO.load_questions('JSON/test_questions.json')
 
+    # Store rules in Knowledge Base
+    kb.set_rules(rules)
+
+    # set known user values so far
     user.set_user_first_name("Mike")
     user.set_user_last_name("Drakos")
 
+    # set known components for working memory
     wm.set_user(user)
-    wm.set_rules(rules)
+    wm.set_rules(kb.get_rules())
     wm.set_phones(phones)
     wm.set_questions(questions)
 
+    # main process for asking questions
     for question in wm.get_questions():
         print(question.get_question())
         for answer in question.get_possible_answers():
@@ -38,8 +50,13 @@ def test_process():
         user_answer = Answer(user_answer_topic, user_answer_content)
         user.set_answer(user_answer)
 
+    # infer answer
     infer = InferenceEngine(wm)
     infer.infer()
+
+    # print answer
+    print(wm.get_user().get_phone().get_model())
+    print(wm.get_user().get_phone().get_cpu())
 
     # user.set_answer("Samsung")
     # user.set_answer(">1000")
@@ -49,6 +66,6 @@ test_process()
 '''
 
 def main():
-    gui.App
+    gui.Window
 
 main()
